6b64727c17c90902e6deb19b51956f0ba8a4c541
Ariel Diaz <arieldiaz92@gmail.com>
2025-09-01 21:55:47 -0400

    Added more weather conditions

--- domain/Enums/WeatherConditions.cs
+++ domain/Enums/WeatherConditions.cs
@@ -2,14 +2,16 @@ namespace Domain.Enums;
 
 public enum WeatherConditions
 {
-    Windy,
-    Cloudy,
-    Sunny,
-    Hot,
-    VeryHot,
-    Cold,
-    VeryCold,
-    Rainy,
-    Foggy,
-    Other
+    Windy = 0,
+    Cloudy = 1,
+    Sunny = 2,
+    Hot = 3,
+    VeryHot = 4,
+    Cold = 5,
+    VeryCold = 6,
+    Rainy = 7,
+    Foggy = 8,
+    Stormy = 9,
+    StrongCurrent = 10,
+    Other = 11
 }
